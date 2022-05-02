e10f1a4429b61a5cdee3463ae992c9ec8eaf312a
kranca <90696292+kranca@users.noreply.github.com>
2022-05-02 10:27:25 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Raúl Carrancá on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
