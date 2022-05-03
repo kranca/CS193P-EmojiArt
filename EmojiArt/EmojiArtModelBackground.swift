3500b72d1b8bd47bcdf98ea999f17301bdfd22e4
kranca <90696292+kranca@users.noreply.github.com>
2022-05-03 17:28:57 +0200

    Lecture 9
    Created MVVM structure and added drag and drop functionality to emoji scrolling pallette

---
+++ EmojiArt/EmojiArtModelBackground.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiArtModelBackground.swift
+//  EmojiArt
+//
+//  Created by Raúl Carrancá on 02/05/22.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
